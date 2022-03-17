f2b9b1b702e25fcfb1ed76868b7cdc8140b117ed
ArthexTheKing <aleandroroma1@gmail.com>
2022-03-17 14:47:23 +0100

    Aggiungere i file di progetto.

---
+++ SheetDice/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SheetDice
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
