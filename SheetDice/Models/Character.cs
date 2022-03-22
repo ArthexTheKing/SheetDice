55314e5c0e9f4211ed8ba8f3ae7830d542c74b75
Alocchi <fra.alocchi@gmail.com>
2022-03-22 18:02:11 +0100

    Bozza Selezione Personaggi
    Creata una bozza della pagina selezione personaggi attraverso una listview di oggetti personaggio .
    La lista non ha nessuna azione associata e contiene solo elementi fissi pregenerati.

---
+++ SheetDice/Models/Character.cs
@@ -0,0 +1,12 @@
+﻿namespace SheetDice.Models
+{
+    public class Character
+    {
+        public string Name { get; set; }
+
+        public Character(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ SheetDice/ViewModels/CharacterSelectionViewModel.cs
@@ -0,0 +1,25 @@
+﻿using MvvmHelpers;
+using MvvmHelpers.Commands;
+using SheetDice.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SheetDice.ViewModels
+{
+    public class CharacterSelectionViewModel : BaseViewModel
+    {
+        public ObservableRangeCollection<Character> Characters { get; }
+        public AsyncCommand RefreshCommand { get; }
+
+        public CharacterSelectionViewModel()
+        {
+            Characters = new ObservableRangeCollection<Character>
+            {
+                new Character("Lilia"),
+                new Character("Albert"),
+                new Character("Baldwin")
+            };
+        }
+    }
+}
