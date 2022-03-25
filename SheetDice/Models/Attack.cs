da3f01aec7b29361e7fa7c2dae42b21c69ff43dd
ValeFalco <val.defalco@stud.uniroma3.it>
2022-03-25 18:26:06 +0100

    Inserimento Lista Attacchi

---
+++ SheetDice/Models/Attack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SheetDice.Models
+{
+    public class Attack
+    {
+        public string Name { get; set; }  //nome dell'arma
+        public string Damage { get; set; } //danno dell'arma
+        public string Type { get; set; } //tipo di danno
+    }
+}
+
---
+++ SheetDice/ViewModels/AttackViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MvvmHelpers;
+using MvvmHelpers.Commands;
+using SheetDice.Models;
+
+namespace SheetDice.ViewModels
+{
+   public class AttackViewModel : BaseViewModel
+    {
+        public ObservableRangeCollection<Attack> Attacks { get; set;}
+
+        public AsyncCommand RefreshComand { get; }
+
+        public AttackViewModel()
+        {
+            Attacks = new ObservableRangeCollection<Attack>
+            {
+                new Attack() { Name = "Broadsword", Damage = "2d6", Type = "Slashing"},
+                new Attack() { Name = "Spear", Damage = "1d6", Type = "Piercing"},
+                new Attack() {Name = "Dagger", Damage = "1d4", Type = "Piercing"}
+            };
+        }
+
+    }
+} 
