6403dbebcaa7e0215010751dcdedf8d99b510e3a
Alocchi <fra.alocchi@gmail.com>
2022-03-25 17:25:53 +0100

    MVVM ListView
    Utilizzato il metodo MVVM per gestire il codice della selezione di un elemento o del menuitem favorite

--- SheetDice/ViewModels/CharacterSelectionViewModel.cs
+++ SheetDice/ViewModels/CharacterSelectionViewModel.cs
@@ -1,10 +1,8 @@
 ﻿using MvvmHelpers;
 using MvvmHelpers.Commands;
 using SheetDice.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace SheetDice.ViewModels
 {
@@ -13,6 +11,8 @@ namespace SheetDice.ViewModels
         public ObservableRangeCollection<Character> Characters { get; }
         public AsyncCommand RefreshCommand { get; }
 
+        public AsyncCommand<Character> FavoriteCommand { get; } 
+        
         public CharacterSelectionViewModel()
         {
             Characters = new ObservableRangeCollection<Character>
@@ -23,8 +23,36 @@ namespace SheetDice.ViewModels
             };
 
             RefreshCommand = new AsyncCommand(Refresh);
+            FavoriteCommand = new AsyncCommand<Character>(Favorite);
 
         }
+
+        async Task Favorite(Character character)
+        {
+            if(character == null) {
+                return; 
+            }
+            await Application.Current.MainPage.DisplayAlert("Favorited", character.Name, "OK");
+        } 
+
+        Character previouslySelected;
+        Character selectedCharacter;
+        public Character SelectedCharacter
+        {
+            get=> selectedCharacter;
+            set
+            {
+                if (value != null)
+                {
+                    Application.Current.MainPage.DisplayAlert("Selected", value.Name, "OK");
+                    previouslySelected = value;
+                    value = null;
+                }
+                selectedCharacter = value;
+                OnPropertyChanged();
+            }
+        }
+
 
         async Task Refresh()
         {
--- SheetDice/Views/CharacterSelection.xaml.cs
+++ SheetDice/Views/CharacterSelection.xaml.cs
@@ -17,29 +17,5 @@ namespace SheetDice.Views
         {
             InitializeComponent();
         }
-
-        private async void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
-        {
-            var character = ((ListView)sender).SelectedItem as Character;
-            if (character == null) {
-                return;
-            }
-            await DisplayAlert("Character selected", character.Name, "ok");
-        }
-
-        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
-        {
-            ((ListView)sender).SelectedItem = null;
-        }
-
-        private async void MenuItem_Clicked(object sender, EventArgs e)
-        {
-            var character = ((MenuItem)sender).BindingContext as Character;
-            if (character == null)
-            {
-                return;
-            }
-            await DisplayAlert("Character favorited", character.Name, "ok");
-        }
     }
 }
