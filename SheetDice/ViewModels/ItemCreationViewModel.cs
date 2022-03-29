1c053dbfd77006d2ac33f6a7f6e08cb10c9ce055
ArthexTheKing <aleandroroma1@gmail.com>
2022-03-29 18:46:53 +0200

    routing di appoggio per la pagina di creazione di oggetti e creazione pagina dedicata alla creazione di oggetti

---
+++ SheetDice/ViewModels/ItemCreationViewModel.cs
@@ -0,0 +1,12 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SheetDice.ViewModels
+{
+    public class ItemCreationViewModel : BaseViewModel
+    {
+
+    }
+}
