b097f60dbdaadac57411aee6f1b46001c5d9e75e
ArthexTheKing <aleandroroma1@gmail.com>
2022-04-07 19:04:03 +0200

    Aggiunta property di ViewModelBase

--- SheetDice/ViewModels/Base/ViewModelBase.cs
+++ SheetDice/ViewModels/Base/ViewModelBase.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SheetDice.ViewModels.Base
 {
-    internal class ViewModelBase
+    public partial class ViewModelBase : ObservableObject
     {
+        [ObservableProperty]
+        private bool isBusy = false;
+
+        [ObservableProperty]
+        private string title = string.Empty;
     }
 }
