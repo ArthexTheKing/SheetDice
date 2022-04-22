26f0712326fee4d35454c4d6289f5782d33c4782
ArthexTheKing <aleandroroma1@gmail.com>
2022-04-22 16:12:11 +0200

    Interfaccia per la gestione del database

---
+++ SheetDice/Services/Repository/IDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SheetDice.Services.Repository
+{
+    public interface IDatabase<T>
+    {
+        public Task Insert(T obj);
+        public Task Update(T obj);
+        public Task Delete(T obj);
+        public Task<T> Get(int id);
+        public Task<IEnumerable<T>> GetAll();
+    }
+}
